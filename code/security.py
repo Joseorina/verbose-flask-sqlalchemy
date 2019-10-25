0ad511b53bcaf16180a9642e7d97388327b2a402
Joseorina <orinangare@gmail.com>
2019-10-25 03:51:12 +0300

    package modification

--- code/security.py
+++ code/security.py
@@ -1,28 +1,28 @@
 from werkzeug.security import safe_str_cmp
-from user import User
+from resources.user import User
 
 
 def authenticate(username, password):
     """
     Generate JWT token
-    
+
     Arguments:
         username {[type]} -- [description]
         password {[type]} -- [description]
-    
+
     Returns:
         [type] -- [description]
     """
     user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
-    
+
 def identity(payload):
     """
-    Authenticate an endpoint    
+    Authenticate an endpoint
     Arguments:
         payload {[type]} -- [description]
-    
+
     Returns:
         [type] -- [description]
     """
