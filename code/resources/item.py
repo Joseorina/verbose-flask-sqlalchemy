632e3afff2f022d2d9e2fb9eca6d81b663a85dea
Joseorina <orinangare@gmail.com>
2019-10-28 17:22:36 +0300

    Displaying ItemList using SQLAlchemy

--- code/resources/item.py
+++ code/resources/item.py
@@ -58,15 +58,6 @@ class Item(Resource):
 
 class Items(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name':row[0], 'price':row[1]})
-
-        connection.close()
-
-        return {'items': items}
+        return {'items': [x.json() for x in ItemModel.query.all()]}
+        #using lambda
+        #return {'items':list(map(lambda x:x.json(),ItemModel.query.all()))}
