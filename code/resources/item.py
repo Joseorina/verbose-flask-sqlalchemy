347dc49cd280b47033259aea09b6ad41d59d8a78
Joseorina <orinangare@gmail.com>
2019-10-28 17:53:14 +0300

    Creating a Store resource

--- code/resources/item.py
+++ code/resources/item.py
@@ -10,6 +10,11 @@ class Item(Resource):
                         required=True,
                         help="This field cannot be blanks"
     )
+    parser.add_argument('store_id',
+                        type=float,
+                        required=True,
+                        help="Every item needs a store id"
+    )
 
 
     @jwt_required()
@@ -25,7 +30,8 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        #item = ItemModel(name, data['price'], data['store_id'])
+        item = ItemModel(name, **data
 
         try:
             item.save_to_db()
@@ -47,7 +53,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
 
