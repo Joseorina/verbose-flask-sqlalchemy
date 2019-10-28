415be878289963cade2d8b1d6af89999bba063f8
Joseorina <orinangare@gmail.com>
2019-10-28 16:42:33 +0300

    implementing item model

--- code/models/item.py
+++ code/models/item.py
@@ -1,9 +1,7 @@
-import sqlite3
 from db import db
 
 
 class ItemModel(db.Model):
-
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -19,34 +17,15 @@ class ItemModel(db.Model):
 
     @classmethod
     def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
+        return cls.query.filter_by(name=name).first()
 
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
+    def save_to_db(self):
+        """
+        insert and update items onto db
+        """
+        db.session.add(self)
+        db.session.commit()
 
-        if row:
-            return  cls(*row)#cls(row[0],row[1])#name, price
-
-    def insert(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES(?,?)"
-        cursor.execute(query, (self.name, self.price))
-
-        connection.commit()
-        connection.close()
-
-    @classmethod
-    def update(self, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (self.price, self.name))
-
-        connection.commit()
-        connection.close()
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
